2ce01e808d9d5768ba6ca61a891d182ac98bb87c
AndreyPysarenko <114475908+AndreyPysarenko@users.noreply.github.com>
2023-07-16 14:13:30 +0300

    second commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('[name="email"]'),
+  textarea: document.querySelector('[name="message"]'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+populateForm();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput() {
+  formData[refs.email.name] = refs.email.value;
+  formData[refs.textarea.name] = refs.textarea.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateForm() {
+  const savedText = localStorage.getItem(STORAGE_KEY);
+  if (savedText) {
+    refs.email.value = JSON.parse(savedText).email;
+    refs.textarea.value = JSON.parse(savedText).message;
+  }
+}
